031a218f5c782ea11b67f877c444b8495c3d9e7f
Marck <myohalmoam@gmail.com>
2026-09-13 18:50:10 -0600

    implemented game logic and achieved mvp state

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -28,6 +28,61 @@ public class Main {
         //Printing initial board on screen:
         printBoard(board);
 
+        while(!terminalState){
+            System.out.println("Current turn is: " + currPlayer);
+            System.out.println("Choose a position (1-9): ");
+            int position = input.nextInt();
+
+
+            //validating position inputed by user
+            boolean validPosition;
+            if (position < 1 || position > 9){
+                validPosition = false;
+            }else {
+                validPosition = true;
+            }
+            //if not valid position, do a loop asking for correct value
+            while(!(validPosition)){
+                System.out.println("The number is not valid, please try another one ");
+                position = input.nextInt();
+                if(position >= 1 && position <= 9){
+                    validPosition = true;
+                }
+            }
+
+            if(validPosition){
+                //Obteniendo el valor de la fila a partir de operacion con input de usuario
+                int row = (int)((position - 1) / 3);
+                //Obteniendo el valor de la columna a partir de operacion con input de usuario
+                int col = (position - 1) % 3;
+
+                //validation of cells not already used.
+                if (board[row][col] != ' '){
+                    System.out.println("Position is already being used. Try another position");
+                    continue;
+                }
+                else {
+                    //asigning an X to an available spot
+                    board[row][col] = currPlayer;
+                }
+            }
+
+            //valid change of players is only if inputed valid position for value
+            if (currPlayer == 'X'){
+                currPlayer = 'O';
+            } else {
+                currPlayer = 'X';
+            }
+
+            //Printing updated board on console:
+            printBoard(board);
+
+            //testing terminal state
+            System.out.println();
+            terminalState = verifyState(board);
+
+        }
+
     }
 
     //Creating board functions
